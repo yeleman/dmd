3eacaf14a2e3f56224b4e20ca182c15126a88750
renaud gaudin <rgaudin@gmail.com>
2015-11-05 14:52:17 +0000

    Fixed #3: validation UI

---
+++ dmd/templatetags/dmd.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_prefix_key(form, prefix, rid, attribute=None):
+    field = form["{p}-{id}".format(p=prefix, id=rid)]
+    if attribute is None:
+        return field
+    attr = getattr(field, attribute)
+    if hasattr(attr, '__call__'):
+        return attr()
+    return attr
--- dmd/views/validation.py
+++ dmd/views/validation.py
@@ -5,16 +5,73 @@
 from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 import logging
+import collections
 
 from django import forms
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.utils.translation import ugettext as _
+
+from dmd.models.DataRecords import DataRecord
+from dmd.models.Entities import Entity
+
+MAX_RESULTS = 50
+MAX_MONTHS = 4
 
 logger = logging.getLogger(__name__)
 
 
 class ValidationForm(forms.Form):
-    pass
+
+    WAIT = DataRecord.NOT_VALIDATED
+    VALIDATED = DataRecord.VALIDATED
+    REJECTED = DataRecord.REJECTED
+    EDIT = 'edit'
+
+    STATUSES = collections.OrderedDict([
+        (WAIT, _("Wait")),
+        (VALIDATED, DataRecord.VALIDATION_STATUSES.get(VALIDATED)),
+        (REJECTED, DataRecord.VALIDATION_STATUSES.get(REJECTED)),
+        (EDIT, _("Edit")),
+    ])
+
+    def __init__(self, *args, **kwargs):
+        records = kwargs.pop('records')
+        super(ValidationForm, self).__init__(*args, **kwargs)
+
+        self.fields = collections.OrderedDict([])
+
+        for record in records:
+            label = "{p}/{l}: {i}".format(
+                p=record.period.strid, l=record.entity,
+                i=record.indicator.name)
+            status_key = 'status-{}'.format(record.id)
+            numerator_key = 'numerator-{}'.format(record.id)
+            denominator_key = 'denominator-{}'.format(record.id)
+            self.fields.update({
+                status_key: forms.ChoiceField(
+                    label=label,
+                    choices=self.STATUSES.items(),
+                    initial=self.initial.get(status_key, self.WAIT)),
+            })
+            self.fields.update({
+                numerator_key: forms.FloatField(
+                    min_value=0,
+                    label=_("Numérateur"),
+                    initial=self.initial.get(numerator_key, record.numerator)),
+            })
+            self.fields.update({
+                denominator_key: forms.FloatField(
+                    min_value=0,
+                    label=_("Dénominateur"),
+                    initial=self.initial.get(denominator_key,
+                                             record.denominator)),
+            })
+
+        for key in self.fields.keys():
+            self.fields[key].widget.attrs['class'] = "form-control"
 
 
 @login_required
@@ -23,16 +80,84 @@ def validation(request, template_name='validation.html'):
 
     context = {'page': 'validation'}
 
+    now = timezone.now()
+    rdc = Entity.get_root()
+    validation_location = request.user.partner.validation_location
+
+    records = DataRecord.objects \
+        .filter(validation_status=DataRecord.NOT_VALIDATED)
+
+    if validation_location != rdc:
+        other_dps = rdc.get_children()
+        other_dps.remove(validation_location)
+        records = records.exclude(entity__in=other_dps) \
+                         .exclude(entity__parent__in=other_dps)
+    # set order
+    records = records.order_by('-created_on')
+
+    nb_total = records.count()
+    records = records[:MAX_RESULTS]
+
     if request.method == 'POST':
-        form = ValidationForm(request.POST, request.FILES)
+        form = ValidationForm(request.POST, records=records)
         if form.is_valid():
-            pass
+            counts = {
+                'untouched': 0,
+                'updated': 0,
+                'validated': 0,
+                'rejected': 0
+            }
+
+            for dr in records:
+                status = form.cleaned_data.get('status-{}'.format(dr.id))
+                if status == form.WAIT:
+                    counts['untouched'] += 1
+                    continue
+
+                if status == 'edit':
+                    numerator = form.cleaned_data.get('numerator-{}'
+                                                      .format(dr.id))
+                    denominator = form.cleaned_data.get('denominator-{}'
+                                                        .format(dr.id))
+
+                    dr.numerator = numerator
+                    dr.denominator = denominator
+                    dr.record_update(request.user.partner)
+
+                    counts['updated'] += 1
+
+                    status = form.VALIDATED
+
+                if status in (form.VALIDATED, form.REJECTED):
+                    if status == form.VALIDATED:
+                        counts['validated'] += 1
+                    else:
+                        counts['rejected'] += 1
+
+                    dr.record_validation(
+                        status=status,
+                        on=now,
+                        by=request.user.partner)
+            messages.info(request, _(
+                "Thank You for your validations. {w} data were left "
+                "untouched, {u} were updated, {v} validated "
+                "and {r} rejected.").format(
+                    w=counts['untouched'],
+                    u=counts['updated'],
+                    v=counts['validated'],
+                    r=counts['rejected']))
+
+            return redirect('validation')
         else:
             # django form validation errors
             pass
     else:
-        form = ValidationForm()
+        form = ValidationForm(records=records)
 
-    context.update({'form': form})
+    context.update({
+        'form': form,
+        'records': records,
+        'nb_total': nb_total
+    })
 
     return render(request, template_name, context)
