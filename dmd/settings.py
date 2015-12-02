b9698e5b0a2f655bbe5e0566b6a868d266e3ffb8
renaud gaudin <rgaudin@gmail.com>
2015-12-02 10:15:20 +0100

    debug back :/

--- dmd/settings.py
+++ dmd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3c+(n%y#c-_$xskc(orm@w=zguxd0%=x1&xy6ri=#zqm_4s$_)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
